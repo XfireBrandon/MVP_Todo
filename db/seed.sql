f471d10ec4a1c03bebbf24afec55ef751d4a12db
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-09-30 13:38:00 -0600

    all backend completed

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO todos (todo) VALUES ('Wash the car');
+INSERT INTO todos (todo) VALUES ('Clean room');
+INSERT INTO todos (todo) VALUES ('Take the trash out');
--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3500;
-// const {pool} = require('pg');
-// const pool = new Pool ({
-//     user: 'brandonmartin',
-//     password: '',
-//     host: 'localhost',
-//     port: 5432,
-//     database:
-// })
+const {Pool} = require('pg');
+const pool = new Pool ({
+    user: 'brandonmartin',
+    password: '',
+    host: 'localhost',
+    port: 5432,
+    database: 'TodoListDB'
+})
 
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cors())
+
+app.get('/todo', async (req, res) => {
+    try {
+        const {rows} = await pool.query('SELECT * FROM todos')
+        res.send(rows)
+    } catch (error){
+        res.send(error.message)
+    }
+})
+
+app.post('/todo/post', async (req, res) => {
+    const {todo} = req.body
+    try {
+        const {rows} = await pool.query('INSERT INTO todos (todo) VALUES ($1) RETURNING *', [todo])
+        res.send(rows)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.patch('/todo/patch/:id', async (req, res) => {
+    const {id} = req.params
+    const {todo} = req.body
+    try {
+        const {rows} = await pool.query('UPDATE todos SET todo = $1 WHERE id = $2 RETURNING *', [todo, id])
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.delete('todo/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const {rows} = await pool.query('DELETE FROM todos WHERE id = $1', [id])
+        res.send(rows)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
 
 
 app.listen(port, () => {
