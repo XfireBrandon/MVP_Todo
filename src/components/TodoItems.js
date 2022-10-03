70e028a6aaf7bfdb36b284904366fb85dbd43d0a
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-10-02 18:22:00 -0600

    line through when done withtodo

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -5,6 +5,11 @@ const TodoItems = ({todoItemProp}) => {
     const handleClick = async (e) => {
         const data = await axios.get(`http://localhost:3500/todo/${e.target.id}`)
         console.log(data.data[0].id)
+        if (e.target.style.textDecoration) {
+            e.target.style.removeProperty('text-decoration');
+          } else {
+            e.target.style.setProperty('text-decoration', 'line-through');
+          }
     }
 
     return(
